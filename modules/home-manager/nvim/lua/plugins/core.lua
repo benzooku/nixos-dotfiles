4008250b2bc19b4bce0507dbd8fa4efee4fcc073
Ben Maurer <benzooku@gmail.com>
2025-09-25 16:53:59 +0200

    nvim changed to LazyVim base from NVChad base

---
+++ modules/home-manager/nvim/lua/plugins/core.lua
@@ -0,0 +1,92 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      auto_install = true,
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "elixir",
+        "css",
+        "heex",
+      },
+
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        html = {
+          filetypes = { "html", "heex" },
+        },
+        cssls = {},
+        ts_ls = {},
+        elixirls = {
+          settings = {
+            elixirLS = {
+              dialyzerEnabled = false,
+              fetchDeps = false,
+            },
+          },
+        },
+        tailwindcss = {
+          filetypes = {
+            "html",
+            "css",
+            "scss",
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "heex", -- add heex here
+          },
+          init_options = {
+            userLanguages = {
+              elixir = "html-eex",
+              heex = "html-eex",
+            },
+          },
+          settings = {
+            tailwindCSS = {
+              includeLanguages = { heex = "html" },
+              experimental = {
+                classRegex = {
+                  -- for Phoenix HEEx templates
+                  'class[:]\\s*"([^"]*)"',
+                  "class[:]\\s*'([^']*)'",
+                  "class[:]\\s*\\[([^]]*)\\]", -- for dynamic classes
+                },
+              },
+            },
+          },
+        },
+        nixd = {},
+      },
+    },
+  },
+}
--- modules/nixos/developement-tools.nix
+++ modules/nixos/developement-tools.nix
@@ -1,74 +1,65 @@
 { config, pkgs, ...}:
 {
-    environment.systemPackages = [
-        # C
-        pkgs.gcc
+  environment.systemPackages = [
+# C
+    pkgs.gcc
 
-        # Nix
-        pkgs.nixd
+# Nix
+      pkgs.nixd
 
-        # General
-        pkgs.inotify-tools
-        pkgs.staruml
-        pkgs.figma-linux
-        pkgs.unzip
-        pkgs.nodejs_24
-        pkgs.watchman
-        pkgs.k6
+# General
+      pkgs.inotify-tools
+      pkgs.staruml
+      pkgs.figma-linux
+      pkgs.unzip
+      pkgs.nodejs_24
+      pkgs.watchman
+      pkgs.k6
 
-        # TUIs and shell tools
-        pkgs.lazygit
-        pkgs.fzf
-        pkgs.ripgrep
-        pkgs.fd
-            
-    ];
+# TUIs and shell tools
+      pkgs.lazygit
+      pkgs.fzf
+      pkgs.ripgrep
+      pkgs.fd
+      pkgs.ast-grep
+      ];
 
-    # KVM
-    programs.virt-manager.enable = true;
-    users.groups.libvirtd.members = ["ben"];
-    virtualisation.libvirtd.enable = true;
-    virtualisation.spiceUSBRedirection.enable = true;
-    # virsh net-start default
-    # to start virt network
+# KVM
+  programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = ["ben"];
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+# virsh net-start default
+# to start virt network
 
-    # waydroid
-    virtualisation.waydroid.enable = true;
+# waydroid
+  virtualisation.waydroid.enable = true;
+# Docker
+  virtualisation.docker.enable = true;
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+  users.users.ben.extraGroups = [ "docker" ];
 
-    
-    # Docker
-    virtualisation.docker.enable = true;
-    virtualisation.docker.rootless = {
-        enable = true;
-        setSocketVariable = true;
-    };
-
-    users.users.ben.extraGroups = [ "docker" ];
-
-    # Postgres
-    services.postgresql = {
-        enable = true;
-        ensureDatabases = [ "postgres" ];
-
-        enableTCPIP = true;
-        authentication = pkgs.lib.mkOverride 10 ''
-      #type database DBuser origin-address auth-method
-    local all       all     trust
-    # ipv4
-    host  all      all     127.0.0.1/32   trust
-    # ipv6
-    host all       all     ::1/128        trust
-        '';
-    };
-
-    # non-nix Executables
-    programs.nix-ld.enable = true;
-
-    programs.nix-ld.libraries = with pkgs; [
-
-        # Add any missing dynamic libraries for unpackaged programs
-
-        # here, NOT in environment.systemPackages
-
-    ];
+# Postgres
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "postgres" ];
+    enableTCPIP = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+#type database DBuser origin-address auth-method
+      local all       all     trust
+# ipv4
+      host  all      all     127.0.0.1/32   trust
+# ipv6
+      host all       all     ::1/128        trust
+      '';
+  };
+# non-nix Executables
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+# Add any missing dynamic libraries for unpackaged programs
+# here, NOT in environment.systemPackages
+  ];
 }
