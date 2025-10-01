6e2c37c2ced371b14f3df62da9e650db1129cabc
Ben Maurer <benzooku@gmail.com>
2025-10-01 17:31:12 +0200

    added rust dev shell

---
+++ shells/rust/flake.nix
@@ -0,0 +1,28 @@
+{
+    description = "Rust Shell";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs }: 
+        let
+            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in 
+            { 
+            devShells.x86_64-linux.default = pkgs.mkShell {
+
+                packages = [
+                    pkgs.cargo
+                    pkgs.rustc
+                ];
+
+                shellHook = ''
+                zsh
+                '';
+
+            };
+
+
+        };
+}
