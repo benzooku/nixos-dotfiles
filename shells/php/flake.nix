f1fab1d380338cbd68e3301cd4d73bd0cc8e6cb1
Ben Maurer <benzooku@gmail.com>
2026-03-10 16:56:52 +0100

    added php develop flake

---
+++ shells/php/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Symfony PHP development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs}:
+    let
+    system = "x86_64-linux";
+  pkgs = import nixpkgs { inherit system; };
+
+  php = pkgs.php.withExtensions ({ enabled, all }: with all; [
+      xsl
+  ]);
+
+  in {
+    devShells.default = pkgs.mkShell {
+      packages = [
+        php
+          pkgs.phpPackages.composer
+          pkgs.symfony-cli
+      ];
+
+      shellHook = ''
+        zsh
+        export PHP_MEMORY_LIMIT=-1
+        echo "PHP development shell ready"
+        php -v
+        '';
+    };
+  };
+}
